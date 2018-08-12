b095fc6185606df5bad35471b7b93c55fe569fcb
Droseratustra <ecila.janne@gmail.com>
2018-08-12 10:36:10 +0100

    PlayerStats Class + Respawn OK + Gamemaster Script
    yep

--- Clingy Critter/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Clingy Critter/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -55,6 +55,8 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward * m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            
+
             newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
             transform.position = newPos;
 
